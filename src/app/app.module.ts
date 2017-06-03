b971eb8d3e73f46df12319d30643d90b9a731f4f
prodbd667 <prodbd667@gmail.com>
2017-06-04 02:32:58 +0300

    add angular material

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,39 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {Routes, RouterModule} from '@angular/router';
+
+import {AppComponent} from './app.component';
+import {WrapComponent} from './wrap/wrap.component';
+import {MainPageComponent} from './main-page/main-page.component';
+import {AboutPageComponent} from './about-page/about-page.component';
+import {NotfoundPageComponent} from './notfound-page/notfound-page.component';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule} from '@angular/material';
+
+const appRoutes: Routes = [
+  {path: '', component: MainPageComponent},
+  {path: 'about', component: AboutPageComponent},
+  {path: '**', component: NotfoundPageComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WrapComponent,
+    MainPageComponent,
+    AboutPageComponent,
+    NotfoundPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
+    BrowserAnimationsModule,
+    MdButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
