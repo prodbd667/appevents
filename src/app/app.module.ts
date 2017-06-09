d9a88bf59d11e11fef1b19e629854f04dab50498
prodbd667 <prodbd667@gmail.com>
2017-06-09 04:47:18 +0300

    add socket

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { Routes, RouterModule } from '@angular/router';
+import {FormsModule} from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { WrapComponent } from './wrap/wrap.component';
-import { MainPageComponent } from './main-page/main-page.component';
-import { AboutPageComponent } from './about-page/about-page.component';
-import { NotfoundPageComponent } from './notfound-page/notfound-page.component';
+import {Routes, RouterModule} from '@angular/router';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MdButtonModule } from '@angular/material';
-import { MdSidenavModule } from '@angular/material';
+import {AppComponent} from './app.component';
+import {WrapComponent} from './wrap/wrap.component';
+import {MainPageComponent} from './main-page/main-page.component';
+import {AboutPageComponent} from './about-page/about-page.component';
+import {NotfoundPageComponent} from './notfound-page/notfound-page.component';
 
-import { DinamicComponent } from './dinamic/dinamic.component';
-import { InsideComponent } from './inside/inside.component';
-import { CalendarComponent } from './calendar/calendar.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MdButtonModule} from '@angular/material';
+import {MdSidenavModule} from '@angular/material';
+import {MdListModule} from '@angular/material';
+
+import {DinamicComponent} from './dinamic/dinamic.component';
+import {InsideComponent} from './inside/inside.component';
+import {CalendarComponent} from './calendar/calendar.component';
+import { SocketComponent } from './socket/socket.component';
+
+import {SocketService} from './services/socket.service';
 
 const appRoutes: Routes = [
-  { path: '', component: MainPageComponent },
-  { path: 'calendar', component: CalendarComponent },
-  { path: 'about', component: AboutPageComponent },
-  { path: '**', component: NotfoundPageComponent }
+  {path: '', component: MainPageComponent},
+  {path: 'calendar', component: CalendarComponent},
+  {path: 'socket', component: SocketComponent},
+  {path: 'about', component: AboutPageComponent},
+  {path: '**', component: NotfoundPageComponent}
 ];
 
 @NgModule({
@@ -33,16 +40,19 @@ const appRoutes: Routes = [
     NotfoundPageComponent,
     DinamicComponent,
     InsideComponent,
-    CalendarComponent
+    CalendarComponent,
+    SocketComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
     BrowserAnimationsModule,
+    FormsModule,
     MdButtonModule,
-    MdSidenavModule
+    MdSidenavModule,
+    MdListModule
   ],
-  providers: [],
+  providers: [SocketService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
